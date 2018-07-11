4625ea596b916e2358f86458cf7346ee30df4617
Nick/Nelson <huggablesquare@gmail.com>
2018-07-10 23:30:10 -0400

    initial commit

---
+++ frontend/index.js
@@ -0,0 +1,87 @@
+import leaflet from 'leaflet';
+
+// have to import these before the css because of a parcel bug
+// https://github.com/parcel-bundler/parcel/issues/879#issuecomment-403263287
+import markerIcon from '../node_modules/leaflet/dist/images/marker-icon.png';
+import markerIcon2x from '../node_modules/leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from '../node_modules/leaflet/dist/images/marker-shadow.png';
+import './styles.css';
+
+console.log(markerIcon, markerIcon2x, markerShadow)
+console.log(leaflet)
+
+function checkStatus(response) {
+	if (response.status >= 200 && response.status < 300) {
+		return response;
+	} else {
+		const error = new Error(response.statusText);
+		error.response = response;
+		throw error;
+	}
+}
+
+function setLoading(huh) {
+	document.querySelector('.loading').style.visibility = huh ? 'visible' : 'hidden';
+}
+
+const osm = leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+const map = leaflet.map('map').setView([37.8, -96], 4).addLayer(osm);
+const group = new leaflet.LayerGroup();
+// can't use default because parcel adds hash to these filenames
+const icon = new leaflet.Icon({
+	iconUrl:       markerIcon,
+	iconRetinaUrl: markerIcon2x,
+	shadowUrl:     markerShadow,
+	iconSize:      [25, 41],
+	iconAnchor:    [12, 41],
+	popupAnchor:   [1, -34],
+	tooltipAnchor: [16, -28],
+	shadowSize:    [41, 41]
+});
+
+function loadUser(username) {
+	setLoading(true);
+	group.clearLayers();
+
+	fetch(`users/${username}`)
+		.then(checkStatus)
+		.then((data) => data.json())
+		.then((data) => {
+			data.forEach((image) => {
+				if (image.has_location && image.location_coords) {
+					console.log('has location')
+					leaflet.marker(image.location_coords.reverse(), { icon })
+					//leaflet.marker(image.location_coords.reverse())
+						.addTo(group)
+						//.bindPopup(`Date Taken: ${new Date(image.capture_date)}`);
+						.bindPopup(`
+							<a href="${image.permalink}"><img width="150" src="//${image.responsive_url}?w=150" /></a><br />
+							<span style="word-wrap: break-word">Date Taken: ${new Date(image.capture_date)}</span>
+						`);
+				}
+			});
+			group.addTo(map);
+			setLoading(false);
+		})
+		.catch((e) => {
+			console.log('uh oh', e);
+		});
+}
+
+const input = document.querySelector('input');
+input.onkeyup = function(event) {
+	if (event.key === 'Enter') {
+		const username = event.target.value;
+		username ? loadUser(username) : group.clearLayers();
+	}
+};
+document.querySelector('.actions > .search').onclick = function() {
+	const username = input.value;
+	username && loadUser(username);
+};
+document.querySelector('.actions > .reset').onclick = function() {
+	group.clearLayers();
+	input.value = '';
+};
---
+++ server.rb
@@ -0,0 +1,42 @@
+require 'sinatra'
+require 'json'
+require 'open-uri'
+require 'securerandom'
+require 'lightly'
+
+lightly = Lightly.new life: 86400
+
+set :public_folder, File.dirname(__FILE__) + '/dist'
+
+get '/users/:user' do
+	content_type :json
+
+	lightly.get params['user'] do
+		puts params['user']
+		user = params['user']
+		initial = open "https://vsco.co/content/Static/userinfo",
+		  'Cookie' => "vs_anonymous_id=#{SecureRandom.uuid}",
+			'Referer' => "https://vsco.co/#{user}/images/1"
+		vs = JSON.parse(initial.read[/{.+}/])['tkn']
+
+		begin
+			sites = JSON.load open "https://vsco.co/ajxp/#{vs}/2.0/sites?subdomain=#{user}", 'Cookie' => "vs=#{vs}"
+			site_id = sites['sites'][0]['id']
+		rescue OpenURI::HTTPError => e
+			return 404, 'user not found' if e.message == "404 Not Found"
+		end
+
+		page = 1
+		size = 1000
+		images = []
+		loop do
+			puts page
+			response = JSON.load open "https://vsco.co/ajxp/#{vs}/2.0/medias?site_id=#{site_id}&page=#{page}&size=#{size}", 'Cookie' => "vs=#{vs}"
+			images.concat response['media']
+			break if response['total'] <= page * size
+			page += 1
+		end
+
+		images.to_json
+	end
+end
