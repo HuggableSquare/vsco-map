02047d0adb6da1eb76a8f34148fb1a79866b15b3
nick <huggablesquare@gmail.com>
2022-03-04 21:58:03 +0000

    Fix authorization issues with vsco api changes

--- server.rb
+++ server.rb
@@ -8,6 +8,13 @@ lightly = Lightly.new life: 86400
 
 set :public_folder, File.dirname(__FILE__) + '/dist'
 
+# bearer token seems to be hardcoded for logged out users
+authorization = "Bearer 7356455548d0a1d886db010883388d08be84d0c9"
+# api seems to 403 on requests with no user-agent
+user_agent = "vsco-map"
+
+headers = { 'Authorization' => authorization, 'User-Agent' => user_agent }
+
 get '/' do
 	send_file File.join(settings.public_folder, 'index.html')
 end
@@ -22,13 +29,9 @@ get '/users/:user' do
 	lightly.get params['user'] do
 		puts params['user']
 		user = params['user']
-		initial = open "https://vsco.co/content/Static/userinfo",
-			'Cookie' => "vs_anonymous_id=#{SecureRandom.uuid}",
-			'Referer' => "https://vsco.co/#{user}/images/1"
-		vs = JSON.parse(initial.read[/{.+}/])['tkn']
 
 		begin
-			sites = JSON.load open "https://vsco.co/ajxp/#{vs}/2.0/sites?subdomain=#{user}", 'Cookie' => "vs=#{vs}"
+			sites = JSON.load open "https://vsco.co/api/2.0/sites?subdomain=#{user}", headers
 			site_id = sites['sites'][0]['id']
 		rescue OpenURI::HTTPError => e
 			return 404, 'user not found' if e.message == "404 Not Found"
@@ -39,7 +42,7 @@ get '/users/:user' do
 		images = []
 		loop do
 			puts page
-			response = JSON.load open "https://vsco.co/ajxp/#{vs}/2.0/medias?site_id=#{site_id}&page=#{page}&size=#{size}", 'Cookie' => "vs=#{vs}"
+			response = JSON.load open "https://vsco.co/api/2.0/medias?site_id=#{site_id}&page=#{page}&size=#{size}", headers
 			images.concat response['media']
 			break if response['total'] <= page * size
 			page += 1
